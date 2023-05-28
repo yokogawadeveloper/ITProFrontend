72762a0917108ea85536bcb17236849b37073456
yokogawadeveloper <ankul.gautam@yokogawa.com>
2023-05-28 14:35:59 +0530

    all desgin and joinee form done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,13 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'procurement',
