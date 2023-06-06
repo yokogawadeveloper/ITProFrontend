9695e1d59bd654dfffe61e1f54859fabfe51dbaf
yokogawadeveloper <ankul.gautam@yokogawa.com>
2023-06-06 14:33:21 +0530

    in menubar added submenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface MenuItem {
+  title: string;
+  link: string;
+  icon: string;
+  submenuItems: SubmenuItem[];
+}
+
+interface SubmenuItem {
+  title: string;
+  link: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public menuItems: MenuItem[] = [
+    {
+      title: 'Procurement',
+      link: '/procurement',
+      icon: 'home-outline',
+      submenuItems: [
+        {
+          title: 'Procurement Request',
+          link: '/procurement',
+          icon: 'analytics-outline',
+        },
+        {
+          title: 'Procurement List',
+          link: '/procurementview',
+          icon: 'bookmark-outline',
+        },
+        {
+          title: 'Procurement Approval',
+          link: '/procurementview',
+          icon: 'bookmark-outline',
+        },
+      ],
+    },
+    {
+      title: 'Menu Item 2',
+      link: '/menu-item-2',
+      icon: 'settings-outline',
+      submenuItems: [
+        {
+          title: 'Submenu Item 3',
+          link: '/submenu-item-3',
+          icon: 'mail-outline',
+        },
+        {
+          title: 'Submenu Item 4',
+          link: '/submenu-item-4',
+          icon: 'person-outline',
+        },
+      ],
+    },
+  ];
+
+  public submenuState: Map<string, boolean> = new Map();
+
   constructor(private router: Router) {}
 
   //
@@ -14,5 +71,14 @@ export class AppComponent {
     sessionStorage.removeItem('currentUser');
     this.router.navigate(['/login']);
   } // logout
+
+  toggleSubmenu(item: MenuItem) {
+    const submenuOpen = this.submenuState.get(item.title) || false;
+    this.submenuState.set(item.title, !submenuOpen);
+  }
+
+  isSubmenuOpen(item: MenuItem): boolean {
+    return this.submenuState.get(item.title) || false;
+  }
 
 }
